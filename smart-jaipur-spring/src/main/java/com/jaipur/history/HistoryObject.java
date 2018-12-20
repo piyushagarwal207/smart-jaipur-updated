56cf09e340ed559469e28d3ebc7a31609976366d
Piyush Agarwal <piyushagarwal207@outlook.com>
2018-12-21 00:37:22 +0530

    History object update with second commit

--- smart-jaipur-spring/src/main/java/com/jaipur/history/HistoryObject.java
+++ smart-jaipur-spring/src/main/java/com/jaipur/history/HistoryObject.java
@@ -1,10 +1,6 @@
 package com.jaipur.history;
 
 public interface HistoryObject {
-	public String place = "";
-	public String time  = "";
-	public String area  = "";
-	public String areaUnit = "";
 	
-	public void setPlace();
+	public void getPlace(String placeid);
 }
--- smart-jaipur-spring/src/main/java/com/jaipur/history/JaipurHistoryBf.java
+++ smart-jaipur-spring/src/main/java/com/jaipur/history/JaipurHistoryBf.java
@@ -1,5 +1,14 @@
 package com.jaipur.history;
 
-public class JaipurHistoryBf {
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+public class JaipurHistoryBf implements HistoryObject {
+
+	@Override
+	public void getPlace(String placeid) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Jaipur History BFunction");
+	}
 	
 }
